5de981d41625832db5148cad767fbd6af5301968
Jack Rosenthal <jack@rosenth.al>
2018-03-07 11:47:30 -0700

    tab completion in iels

--- iels.py
+++ iels.py
@@ -3,6 +3,7 @@
 
 import sys
 import readline
+from functools import reduce
 from elephant import els, parse, unparse
 
 def interact():
@@ -12,6 +13,31 @@ def interact():
         readline.insert_text(' '.join(unparse(els.stack)) + (' ' if els.stack else ''))
         els.stack = []
 
+    def completer(text, state):
+        def gen():
+            variables = reduce(set.union, map(dict.keys, els.vars), set())
+            for s in '%', '$':
+                for v in variables:
+                    if (s + v).startswith(text):
+                        yield s + v
+            for op in els.operators:
+                if op.startswith(text):
+                    yield op
+            for syntax in 'begin', 'end':
+                if syntax.startswith(text):
+                    yield syntax
+
+        if state == 0:
+            completer.it = gen()
+
+        try:
+            return next(completer.it)
+        except StopIteration:
+            return None
+
+    readline.set_completer(completer)
+    readline.set_completer_delims(' ')
+    readline.parse_and_bind("tab: complete")
     readline.set_startup_hook(input_hook)
     while True:
         try:
